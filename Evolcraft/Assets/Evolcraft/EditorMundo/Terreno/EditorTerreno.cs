a970b9d1aab6f7d554e93f1a75de5aba47a724b7
josue <josue.ccama@ucsp.edu.pe>
2015-09-23 13:59:11 -0500

    Subiendo Anexos
    Subiendo Terreno Corregido

---
+++ Evolcraft/Assets/Evolcraft/EditorMundo/Terreno/EditorTerreno.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Cubiquity;
+using Cubiquity.Impl;
+public class EditorTerreno : MonoBehaviour {
+
+	int numObjects = 1;
+	public GameObject BaldosaPF;
+	public GameObject CubePF;	
+	int depth = 100;
+	int height = 10;
+	int width = 100;
+
+	enum DIRECCIONES
+	{
+		ADELANTE,
+		ATRAS,
+		IZQUIERDA,
+		DERECHA,
+		ARRIBA,
+		ABAJO,
+		NUM_DIRECCIONES
+	};
+
+	public int estado = (int)DIRECCIONES.ADELANTE;
+
+	void Start () {
+		GameObject b1 = Instantiate (BaldosaPF) as GameObject;
+		b1.name="0#0#0";
+		b1.GetComponent<TerrainVolume>().data = VolumeData.CreateFromVoxelDatabase<TerrainVolumeData>(@"C:\Cat\0#0#0.vdb",VolumeData.WritePermissions.ReadWrite);
+		GameObject cont1 = new GameObject();
+		GameObject c1 = Instantiate (CubePF) as GameObject;
+		c1.name = "cubo";
+		b1.transform.parent = cont1.transform;
+		c1.transform.parent = cont1.transform;
+		c1.transform.localScale = new Vector3 (10, 10, 10);
+
+		Vector3i esqInferior = b1.GetComponent<TerrainVolume> ().data.enclosingRegion.lowerCorner;
+		Vector3i esqSuperior = b1.GetComponent<TerrainVolume> ().data.enclosingRegion.upperCorner;
+
+		Vector3 baldosPos = b1.transform.position;
+		Vector3 newPos = new Vector3 ( baldosPos.x+(esqSuperior.x-esqInferior.x)/2, baldosPos.y+(esqSuperior.y-esqInferior.y),baldosPos.z+(esqSuperior.z-esqInferior.z)/2 );
+
+		c1.transform.position = newPos;
+	}
+	
+	TerrainVolumeData crearVolumenData(string direccion)
+	{
+		TerrainVolumeData volumeData = VolumeData.CreateEmptyVolumeData<TerrainVolumeData>(new Region(0, 0, 0, width - 1, height - 1, depth - 1),@"C:\CS-UCSP\Evolcraft\"+ direccion+".vdb");
+		numObjects++;
+		//Si no recibe un path, crea solo un temporal. Caso contrario, evalua si es un path relativo o no...
+		MaterialSet materialSet = new MaterialSet();
+
+		for (int z = 0; z < depth; z++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					materialSet.weights[0] = (byte)255;  
+					volumeData.SetVoxel(x, y, z, materialSet);
+				}
+			}
+		}
+		volumeData.CommitChanges ();
+
+		return volumeData;
+	}
+
+	int[] codigo(string name){
+		int[] codes = new int[3];
+		int ind1, ind2;
+		ind1 = 0;
+		ind2 = name.IndexOf("#");
+		Debug.Log ("ind2: "+ind2);
+		for(int i=0;i<2;i++){
+			codes[i] = Int32.Parse(name.Substring(ind1,ind2-ind1));
+			ind1 = ind2+1;
+			ind2 = name.IndexOf("#",ind1);
+		}
+		codes[2] = Int32.Parse(name.Substring(ind1,name.Length-ind1));
+		return codes;
+	}
+
+
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) 
+			{
+//				Debug.Log (hit.collider.gameObject.name);
+				if(hit.collider.gameObject.name == "cubo"){
+
+					string name = hit.collider.gameObject.transform.parent.GetChild(0).name;
+					Debug.Log("nombre: "+name);
+					GameObject chocado = GameObject.Find(name);
+					Vector3 posChocado = chocado.transform.position;
+
+					int[] codes = codigo(name);
+
+					GameObject cont1 = new GameObject();
+
+					Vector3 newBaldosaPos = new Vector3();
+
+					switch(estado)
+					{
+					case (int)DIRECCIONES.ADELANTE: 	newBaldosaPos = new Vector3(posChocado.x,posChocado.y,posChocado.z+depth);
+												codes[2]++;
+							break;
+					case (int)DIRECCIONES.ATRAS: newBaldosaPos = new Vector3(posChocado.x,posChocado.y,posChocado.z-depth);
+												codes[2]--;
+							break;
+					case (int)DIRECCIONES.ARRIBA: newBaldosaPos = new Vector3(posChocado.x,posChocado.y+height,posChocado.z);
+												codes[1]++;
+							break;
+					case (int)DIRECCIONES.ABAJO: newBaldosaPos = new Vector3(posChocado.x,posChocado.y-height,posChocado.z+depth);
+												codes[1]--;				
+							break;
+					case (int)DIRECCIONES.DERECHA: newBaldosaPos = new Vector3(posChocado.x+width,posChocado.y,posChocado.z);
+												codes[0]++;
+							break;
+					case (int)DIRECCIONES.IZQUIERDA: newBaldosaPos = new Vector3(posChocado.x-width,posChocado.y,posChocado.z+depth);
+												codes[0]--;
+							break;
+					}
+					string codepath="";
+					codepath += codes[0].ToString()+"#"+codes[1].ToString()+"#"+codes[2].ToString();
+
+
+					GameObject b1 = Instantiate (BaldosaPF) as GameObject;
+					b1.name = codepath;
+					b1.GetComponent<TerrainVolume>().data = crearVolumenData(codepath);
+					GameObject c1 = Instantiate (CubePF) as GameObject;
+					c1.name="cubo";
+
+					b1.transform.position = newBaldosaPos;
+
+					b1.transform.parent = cont1.transform;
+					c1.transform.parent = cont1.transform;
+
+					c1.transform.localScale = new Vector3 (10, 10, 10);
+					
+					Vector3i esqInferior = b1.GetComponent<TerrainVolume> ().data.enclosingRegion.lowerCorner;
+					Vector3i esqSuperior = b1.GetComponent<TerrainVolume> ().data.enclosingRegion.upperCorner;
+					
+					Vector3 baldosPos = b1.transform.position;
+					Vector3 newPos = new Vector3 ( baldosPos.x+(esqSuperior.x-esqInferior.x)/2, baldosPos.y+(esqSuperior.y-esqInferior.y),baldosPos.z+(esqSuperior.z-esqInferior.z)/2 );
+					
+					c1.transform.position = newPos;
+				}
+			}
+
+		}else if(Input.GetKeyDown(KeyCode.Alpha1)){
+				estado = (int)DIRECCIONES.ADELANTE;
+		}else if(Input.GetKeyDown(KeyCode.Alpha2)){
+			estado = (int)DIRECCIONES.ATRAS;
+		}else if(Input.GetKeyDown(KeyCode.Alpha3)){
+			estado = (int)DIRECCIONES.ARRIBA;
+		}else if(Input.GetKeyDown(KeyCode.Alpha4)){
+			estado = (int)DIRECCIONES.ABAJO;
+		}else if(Input.GetKeyDown(KeyCode.Alpha5)){
+			estado = (int)DIRECCIONES.IZQUIERDA;
+		}else if(Input.GetKeyDown(KeyCode.Alpha6)){
+			estado = (int)DIRECCIONES.DERECHA;
+		}  
+		
+	}
+}
---
+++ Evolcraft/Assets/Evolcraft/EditorMundo/Terreno/EditorTerrenos.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cubiquity;
+using Cubiquity.Impl;
+using UnityEditor;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+public class EditorTerrenos : MonoBehaviour {
+
+    //aun falta la serializacion
+    enum DIRECCIONES
+    {
+        ADELANTE,
+        ATRAS,
+        IZQUIERDA,
+        DERECHA,
+        ARRIBA,
+        ABAJO,
+        NUM_DIRECCIONES
+    };
+
+
+    //Tamaño del voxel
+    int Profundidad   = 100;
+    int Altura       = 32;
+    int Ancho        = 100;
+    //Prefab
+    public GameObject VoxelPrefab;
+
+    //Variables Mapa
+    static string DirectorioLocal = Application.dataPath+"/Evolcraft/EditorMundo/Mapas/";
+    static string NombreMapa="MapaDemo"; //Nombre de la carpeta del mapa
+
+    
+    //Voxels
+    Dictionary<string, GameObject>  MapaVoxels= new Dictionary<string, GameObject>();
+    GameObject                      VoxelActual;
+    Vector3                         CentroActual;
+    string                          ClaveActual;
+    static DIRECCIONES Lado =       DIRECCIONES.ADELANTE;
+
+
+    void Start ()
+    {
+         AssetDatabase.CreateFolder("Assets/Evolcraft/EditorMundo/Mapas", NombreMapa);
+        CrearVoxel(0,0,0,"0#0#0");
+    }
+
+    bool CrearVoxel(float px, float py, float pz, string pnombre)
+    {
+        
+        //Creacion del voxel la db
+        TerrainVolumeData volumeData = VolumeData.CreateEmptyVolumeData<TerrainVolumeData>(new Region(0, 0, 0, Ancho - 1, Altura - 1, Profundidad - 1), @DirectorioLocal + NombreMapa+"/"+pnombre + ".vdb");
+        MaterialSet materialSet = new MaterialSet();
+
+        double DifAltura = Altura - Altura * 0.9;
+        for (int z = 0; z < Profundidad; z++)
+        {
+            for (int y = 0; y < (int)DifAltura; y++)
+            {
+                for (int x = 0; x < Ancho; x++)
+                {
+                    materialSet.weights[0] = (byte)255;
+                    volumeData.SetVoxel(x, y, z, materialSet);
+                }
+            }
+        }
+        volumeData.CommitChanges();
+        //asignacion del prefab
+
+        GameObject VoxelTemporal = Instantiate(VoxelPrefab) as GameObject;
+        VoxelTemporal.name = pnombre;
+        VoxelTemporal.transform.GetChild(0).GetComponent<TerrainVolume>().data=volumeData;
+       VoxelTemporal.transform.GetChild(1).Translate(Ancho/2, (float)DifAltura, Profundidad/2);
+
+        VoxelTemporal.transform.position = new Vector3(px,py,pz);
+        
+        MapaVoxels.Add(pnombre, VoxelTemporal);
+            VoxelActual = VoxelTemporal;
+            ClaveActual = pnombre;
+        
+        return false;
+    }
+
+    // Update is called once per frame
+    string PrepararNombre()
+    {
+        string[] Pos = ClaveActual.Split('#');
+
+        int nx= Int32.Parse(Pos[0]); 
+        int ny= Int32.Parse(Pos[1]);
+        int nz= Int32.Parse(Pos[2]);
+
+        string NuevoNombre;
+        switch(Lado)
+        {
+            case DIRECCIONES.ATRAS:
+                nz = nz - 1;
+                break;
+            case DIRECCIONES.ADELANTE:
+                nz = nz + 1;
+                break;
+            case DIRECCIONES.IZQUIERDA:
+                nx = nx - 1;
+                break;
+            case DIRECCIONES.DERECHA:
+                nx = nx + 1;
+                break;
+            case DIRECCIONES.ARRIBA:
+                ny = ny - 1;
+                break;
+            case DIRECCIONES.ABAJO:
+                ny = ny + 1;
+                break;
+        }
+        NuevoNombre = nx.ToString() + "#" + ny.ToString() + "#" + nz.ToString();
+        return NuevoNombre;
+    }
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.name == "Centro")
+            {
+                Vector3 PosTemp = hit.collider.gameObject.transform.position;
+                ClaveActual = hit.collider.gameObject.transform.parent.transform.name;
+                CentroActual = hit.collider.gameObject.transform.parent.position;
+            }
+
+        }
+            if (Input.GetMouseButtonDown(0) && hit.collider.gameObject.name == "Centro")
+        {
+            Vector3 NuevaPosicion=CentroActual;
+
+
+            switch (Lado)
+            {
+                case DIRECCIONES.ATRAS:
+                    NuevaPosicion.z =NuevaPosicion.z - Profundidad ;
+                    break;
+                case DIRECCIONES.ADELANTE:
+                    NuevaPosicion.z = NuevaPosicion.z + Profundidad;
+                    break;
+                case DIRECCIONES.IZQUIERDA:
+                    NuevaPosicion.x = NuevaPosicion.x - Ancho ;
+                    break;
+                case DIRECCIONES.DERECHA:
+                    NuevaPosicion.x = NuevaPosicion.x + Ancho ;
+                    break;
+                case DIRECCIONES.ARRIBA:
+                    NuevaPosicion.y = NuevaPosicion.y + Altura ;
+                    break;
+                case DIRECCIONES.ABAJO:
+                    NuevaPosicion.y = NuevaPosicion.y - Altura ;
+                    break;
+            }
+            if (!MapaVoxels.ContainsKey(PrepararNombre()))
+            {
+                CrearVoxel(NuevaPosicion.x, NuevaPosicion.y, NuevaPosicion.z, PrepararNombre());
+            }
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Keypad2)){
+				Lado =DIRECCIONES.ADELANTE;
+		}else if(Input.GetKeyDown(KeyCode.Keypad8)){
+            Lado = DIRECCIONES.ATRAS;
+		}else if(Input.GetKeyDown(KeyCode.Keypad5)){
+            Lado = DIRECCIONES.ARRIBA;
+		}else if(Input.GetKeyDown(KeyCode.Keypad0)){
+            Lado = DIRECCIONES.ABAJO;
+		}else if(Input.GetKeyDown(KeyCode.Keypad4)){
+            Lado = DIRECCIONES.IZQUIERDA;
+		}else if(Input.GetKeyDown(KeyCode.Keypad6)){
+            Lado = DIRECCIONES.DERECHA;
+		}  
+    }
+}
